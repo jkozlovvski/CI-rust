3ef589c7b1aa2a4075a2e1a60f2694111ac22bf6
Julian Kozlowski <juliankozlowski@MacBook-Pro-Julian.local>
2022-12-24 11:47:40 +0100

    finished repo_observer, not tested

--- src/repo_observer/lib.rs
+++ src/repo_observer/lib.rs
@@ -1,5 +1,7 @@
+use serde::{Deserialize, Serialize};
+use std::io::Write;
+use std::net::{Ipv4Addr, SocketAddrV4, TcpStream};
 use std::{env, fs};
-use std::net::{SocketAddrV4, Ipv4Addr};
 
 pub fn get_path_to_script() -> String {
     match env::var("UPDATE_SCRIPT_PATH") {
@@ -17,7 +19,7 @@ pub fn read_commit_id(commit_path: String) -> String {
 
 pub struct DispatcherConfig {
     pub socket: SocketAddrV4,
-    pub repository_path: String
+    pub repository_path: String,
 }
 
 impl DispatcherConfig {
@@ -27,21 +29,89 @@ impl DispatcherConfig {
         let dispatcher_host: (u8, u8, u8, u8) = match args.next() {
             Some(arg) => {
                 let mut host = arg.split(".");
-                (host.next().unwrap().parse::<u8>().unwrap(), host.next().unwrap().parse::<u8>().unwrap(), host.next().unwrap().parse::<u8>().unwrap(), host.next().unwrap().parse::<u8>().unwrap())
-            },
-            None => (127, 0, 0, 1)
+                (
+                    host.next().unwrap().parse::<u8>().unwrap(),
+                    host.next().unwrap().parse::<u8>().unwrap(),
+                    host.next().unwrap().parse::<u8>().unwrap(),
+                    host.next().unwrap().parse::<u8>().unwrap(),
+                )
+            }
+            None => (127, 0, 0, 1),
         };
 
         let dispatcher_port = match args.next() {
             Some(arg) => arg.parse::<u16>().unwrap(),
-            None => 8080 
+            None => 8080,
         };
-            
+
         let repository_path = match args.next() {
             Some(arg) => arg,
             None => String::from("/Users/juliankozlowski/Desktop/Studia/Rust/continous_integration_tool/src/repo_observer/test_repo_clone_obs")
         };
 
-        DispatcherConfig {socket: SocketAddrV4::new(Ipv4Addr::new(dispatcher_host.0, dispatcher_host.1, dispatcher_host.2, dispatcher_host.3), dispatcher_port), repository_path}
+        DispatcherConfig {
+            socket: SocketAddrV4::new(
+                Ipv4Addr::new(
+                    dispatcher_host.0,
+                    dispatcher_host.1,
+                    dispatcher_host.2,
+                    dispatcher_host.3,
+                ),
+                dispatcher_port,
+            ),
+            repository_path,
+        }
+    }
+}
+
+pub struct DispatcherConnection {
+    dispatcher_socket: SocketAddrV4,
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum Request {
+    CheckStatus,
+    Update(String),
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum Response {
+    Ok,
+    Error,
+}
+
+impl DispatcherConnection {
+    pub fn new(dispatcher_socket: SocketAddrV4) -> DispatcherConnection {
+        DispatcherConnection { dispatcher_socket }
+    }
+
+    fn send_massage(&self, request: Request) -> Response {
+        match TcpStream::connect(self.dispatcher_socket) {
+            Ok(mut stream) => {
+                serde_json::to_writer(&stream, &request).unwrap();
+                stream.flush().unwrap();
+                let mut deserializer =
+                    serde_json::Deserializer::from_reader(stream.try_clone().unwrap());
+                match Response::deserialize(&mut deserializer) {
+                    Ok(_) => Response::Ok,
+                    Err(err) => {
+                        println!("Error while reading response: {:?}", err);
+                        std::process::exit(1);
+                    }
+                }
+            }
+            Err(error) => {
+                println!("Error: {:?}", error);
+                std::process::exit(1);
+            }
+        }
+    }
+
+    pub fn check_status(&self) -> Response {
+        self.send_massage(Request::CheckStatus)
+    }
+
+    pub fn update(&self, commit_id: String) -> Response {
+        self.send_massage(Request::Update(commit_id))
     }
 }
--- src/repo_observer/repo_observer.rs
+++ src/repo_observer/repo_observer.rs
@@ -2,38 +2,43 @@ use repo_observer_lib::*;
 use std::env;
 use std::path::Path;
 use std::process::Command;
-use std::time::Duration;
 use std::thread::sleep;
+use std::time::Duration;
 
 fn main() {
     let config = DispatcherConfig::build(env::args());
     let script_path = get_path_to_script();
     let commit_path = Path::new(".commit_id");
+    let dispatcher_connection = DispatcherConnection::new(config.socket);
 
     loop {
-        println!("{}", String::from("bash") + " " + &script_path);
+        let command_result = Command::new(&script_path)
+            .arg(&config.repository_path)
+            .arg(&config.socket.ip().to_string())
+            .arg(&config.socket.port().to_string())
+            .output();
 
-        let command_result = Command::new(String::from("bash") + " " + &script_path)
-                .arg(&config.repository_path)
-                .arg(&config.socket.ip().to_string())
-                .arg(&config.socket.port().to_string())
-                .output();
+        match command_result {
+            Ok(_) => {
+                if commit_path.exists() {
+                    if let Response::Error = dispatcher_connection.check_status() {
+                        println!("Error while checking dispatcher status");
+                        std::process::exit(1);
+                    }
 
-        println!("{:?}", command_result);
+                    let commit_id = read_commit_id(commit_path.to_str().unwrap().to_string());
+                    if let Response::Error = dispatcher_connection.update(commit_id) {
+                        println!("Error while updating dispatcher");
+                        std::process::exit(1);
+                    }
+                }
+            }
+            Err(err) => {
+                println!("Error while executing script: {:?}", err);
+                std::process::exit(1);
+            }
+        }
 
         sleep(Duration::from_secs(5));
-
-        // match command_result {
-        //     Ok(output) => {
-        //         if (commit_path.exists()) {
-                    
-        //         }
-        //     },
-        //     Err(err) => {
-        //         println!("Error while executing script: {:?}", err);
-        //         std::process::exit(1);
-        //     }
-        // }
-
     }
 }
