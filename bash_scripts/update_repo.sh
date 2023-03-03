23f5e0a9579eaa329ed5a113c0487da964ee43a5
Julian Kozłowski <kozlovvski@Julians-MacBook-Air.local>
2023-03-03 11:32:53 +0100

    More fixing

--- bash_scripts/update_repo.sh
+++ bash_scripts/update_repo.sh
@@ -12,6 +12,7 @@ if [ $? != 0 ]; then
 fi
 COMMIT_ID=`echo $COMMIT | awk '{ print $2 }'`
 
+git stash
 git checkout main
 run_or_fail "Could not pull from repository" git pull
 COMMIT=$(run_or_fail "Could not call 'git log' on repository" git log -n1)
